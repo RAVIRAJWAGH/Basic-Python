98e897d30ef366d576630508a578540c64cbce86
RAVIRAJ PRAVIN WAGH <103710873+RAVIRAJWAGH@users.noreply.github.com>
2023-04-09 19:44:54 +0530

    Add files via upload

---
+++ label encoding feature scaling.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+data=pd.read_csv(r"C:\Users\Parimal\Desktop\chegg 23122022.csv")
+data
+
+
+# # Label Encoding
+
+# In[3]:
+
+
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+data['E']=le.fit_transform(data['E'])
+
+
+# In[4]:
+
+
+data
+
+
+# In[5]:
+
+
+independent_features=data.iloc[:,:-1].values
+dependent_feature=data.iloc[:,-1].values
+independent_features
+
+
+# In[6]:
+
+
+from imblearn.over_sampling import RandomOverSampler
+ro=RandomOverSampler()
+x_data,y_data=ro.fit_resample(independent_features,dependent_feature)
+
+from collections import Counter
+print(Counter(y_data))
+
+
+# # Feature Scalling
+
+# In[7]:
+
+
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+x_data=sc.fit_transform(x_data)
+
+
+# In[8]:
+
+
+# Train test Spite
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x_data,y_data)
+
+
+# In[9]:
+
+
+from sklearn.tree import DecisionTreeClassifier
+dt=DecisionTreeClassifier(criterion='gini')
+dt.fit(x_train,y_train)
+y_pred=dt.predict(x_test)
+from sklearn.metrics import accuracy_score
+accuracy=accuracy_score(y_test,y_pred)
+accuracy
+
+
+# In[ ]:
+
+
+
+
